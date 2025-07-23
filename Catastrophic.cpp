22d10decc5c317ba0bc268b9bf1f10c6a1f88f2c
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-07-24 00:25:50 +0600

    \Catastrophic.cpp

---
+++ Catastrophic.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello new one welcome this planet , this planet always catastrophic your positive mind" << endl;
+    return 0;
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
